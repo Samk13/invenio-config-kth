e852b14cab63794c18d880dcfae54b603f464d48
Sam AL-Arbid <samaa2@kth.se>
2022-10-18 13:20:06 +0200

    test: pass tests

--- invenio_subjects_cessda/config.py
+++ invenio_subjects_cessda/config.py
@@ -5,7 +5,7 @@
 # invenio-subjects-CESSDA is free software, you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file details.
 
-"""CESSDA vocabularies endpoints"""
+"""CESSDA vocabularies endpoints."""
 urls = [
     {
         "name": "Aggregation Method",
