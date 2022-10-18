e69f1e829b9638b09ff6b42cbcad44c18a8e0055
Sam AL-Arbid <samaa2@kth.se>
2022-10-18 12:20:24 +0200

    fix: capitalization and add spaces to names

--- invenio_subjects_cessda/config.py
+++ invenio_subjects_cessda/config.py
@@ -1,119 +1,126 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 KTH Royal Institute of Technology Sweden
+#
+# invenio-subjects-CESSDA is free software, you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file details.
+
 """CESSDA vocabularies endpoints"""
 urls = [
     {
-        "name": "aggregationmethod",
+        "name": "Aggregation Method",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/aggregationmethod/1.1/en",
     },
     {
-        "name": "analysisunit",
+        "name": "Analysis Unit",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/analysisunit/2.1/en",
     },
     {
-        "name": "cdcPublishernames",
+        "name": "CDC Publisher Names",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/cdcPublishernames/5.0/en",
     },
     {
-        "name": "CESSDAPersistentIdentifierTypes",
+        "name": "CESSDA Persistent Identifier Types",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/CESSDAPersistentIdentifierTypes/1.0/en",
     },
     {
-        "name": "topicclassification",
+        "name": "CESSDA Topic Classification",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/topicclassification/4.2/en",
     },
     {
-        "name": "CharacterSet",
+        "name": "Character Set",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/CharacterSet/1.0/en",
     },
     {
-        "name": "CommonalityType",
+        "name": "Commonality Type",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/CommonalityType/1.0/en",
     },
     {
-        "name": "ContributorRole",
+        "name": "Contributor Role",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/ContributorRole/1.0/en",
     },
     {
-        "name": "CountryNamesandCodes",
+        "name": "Country Names and Codes",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/CountryNamesandCodes/1.0/en",
     },
     {
-        "name": "DataSourceType",
+        "name": "Data Source Type",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/DataSourceType/1.0/en",
     },
     {
-        "name": "DataType",
+        "name": "Data Type",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/DataType/1.1/en",
     },
     {
-        "name": "DateType",
+        "name": "Date Type",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/DateType/1.1/en",
     },
     {
-        "name": "GeneralDataFormat",
+        "name": "General Data Format",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/GeneralDataFormat/2.0/en",
     },
     {
-        "name": "LanguageProficiency",
+        "name": "Language Proficiency",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/LanguageProficiency/1.0/en",
     },
     {
-        "name": "LifecycleEventType",
+        "name": "Lifecycle Event Type",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/LifecycleEventType/1.0/en",
     },
     {
-        "name": "ModeOfCollection",
+        "name": "Mode Of Collection",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/ModeOfCollection/4.0/en",
     },
     {
-        "name": "NumericType",
+        "name": "Numeric Type",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/NumericType/1.1/en",
     },
     {
-        "name": "ResponseUnit",
+        "name": "Response Unit",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/ResponseUnit/1.0/en",
     },
     {
-        "name": "samplingprocedure",
+        "name": "Sampling Procedure",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/samplingprocedure/1.1/en",
     },
     {
-        "name": "SoftwarePackage",
+        "name": "Software Package",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/SoftwarePackage/1.0/en",
     },
     {
-        "name": "SummaryStatisticType",
+        "name": "Summary Statistic Type",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/SummaryStatisticType/2.1/en",
     },
     {
-        "name": "TimeMethod",
+        "name": "Time Method",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/TimeMethod/1.2/en",
     },
     {
-        "name": "TimeZone",
+        "name": "Time Zone",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/TimeZone/1.0/en",
     },
     {
-        "name": "TypeofAddress",
+        "name": "Type of Address",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/TypeofAddress/1.1/en",
     },
     {
-        "name": "TypeofConceptGroup",
+        "name": "Type of Concept Group",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/TypeofConceptGroup/1.0/en",
     },
     {
-        "name": "TypeofInstrument",
+        "name": "Type of Instrument",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/TypeofInstrument/1.1/en",
     },
     {
-        "name": "TypeofNote",
+        "name": "Type of Note",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/TypeofNote/1.1/en",
     },
     {
-        "name": "TypeofTelephone",
+        "name": "Type of Telephone",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/TypeofTelephone/1.0/en",
     },
     {
-        "name": "TypeofTranslationMethod",
+        "name": "Type of Translation Method",
         "endpoint": "https://vocabularies.cessda.eu/v2/codes/TypeofTranslationMethod/1.0/en",
     },
 ]
