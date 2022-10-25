4f1c69afecf8a4d7b8882b27a27c2b4ce010ee55
Sam AL-Arbid <samaa2@kth.se>
2022-10-25 11:43:58 +0200

    test:  fix path

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -11,7 +11,7 @@ from tests.fixtures import create_json_path, create_yaml_path
 
 def test_write2yaml(create_yaml_path):
     """Test write2yaml"""
-    voc_path = f"{os.getcwd()}/tests/test.yaml"
+    voc_path = create_yaml_path
     write2yaml(("Mailing address", test_data[0]), voc_path, cessda_schema)
     with open(voc_path, "r", encoding="utf-8") as f:
         data = yaml.load(f, Loader=SafeLoader)
@@ -21,7 +21,7 @@ def test_write2yaml(create_yaml_path):
 
 def test_write_voc(create_json_path):
     """Test write json"""
-    voc_path = f"{os.getcwd()}/tests/test.json"
+    voc_path = create_json_path
     write_voc(voc_path, test_data, "w+")
     with open(voc_path, "r", encoding="utf-8") as f:
         data = f.read()
