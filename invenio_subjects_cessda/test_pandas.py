674b936051af6a058b4ed04d39e2479e403b191f
Sam Arbid <sam.al-arbid@capgemini.com>
2022-10-30 21:44:35 +0100

    config: add pipenv  support

--- invenio_subjects_cessda/test_pandas.py
+++ invenio_subjects_cessda/test_pandas.py
@@ -1,20 +1,20 @@
-#%%
-from os import getcwd
-import json
-import pandas as pd
+# #%%
+# import json
+# from os import getcwd
 
+# import pandas as pd
 
-path = f'{getcwd()}/vocabularies/result.json'
-res = pd.read_json(path)
+# path = f"{getcwd()}/vocabularies/result.json"
+# res = pd.read_json(path)
 
-with open(path,'r', encoding="utf-8") as f:
-    data = json.loads(f.read())
+# with open(path, "r", encoding="utf-8") as f:
+#     data = json.loads(f.read())
 
-# Flattening JSON data
-df = pd.json_normalize(data, record_path=['data'], meta=['name'])[['id', 'name', 'title', 'uri']]
+# # Flattening JSON data
+# df = pd.json_normalize(data, record_path=["data"], meta=["name"])[
+#     ["id", "name", "title", "uri"]
+# ]
 
-df
+# df
 
-# ff.head
-
-
+# # ff.head
