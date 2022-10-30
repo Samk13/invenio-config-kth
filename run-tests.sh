0e6b07482f31f4bc7f473704cf226a1282132417
Sam Arbid <sam.al-arbid@capgemini.com>
2022-10-30 21:39:30 +0100

    config: fix test error on mac

--- run-tests.sh
+++ run-tests.sh
@@ -11,22 +11,9 @@ set -o errexit
 # Quit on unbound symbols
 set -o nounset
 
-pytest_args=()
-for arg in $@; do
-	# from the CLI args, filter out some known values and forward the rest to "pytest"
-	# note: we don't use "getopts" here b/c of some limitations (e.g. long options),
-	#       which means that we can't combine short options (e.g. "./run-tests -Kk pattern")
-	case ${arg} in
-		*)
-			pytest_args+=( ${arg} )
-			;;
-	esac
-done
-
-
 python -m check_manifest --no-build-isolation
 # Note: expansion of pytest_args looks like below to not cause an unbound
 # variable error when 1) "nounset" and 2) the array is empty.
-python -m pytest ${pytest_args[@]}
+python -m pytest $@
 tests_exit_code=$?
 exit "$tests_exit_code"
