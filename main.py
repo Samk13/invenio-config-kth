a1d8b94620f77bbe6ec636fc37e6d885cd1b2371
Sam AL-Arbid <samaa2@kth.se>
2022-11-08 11:41:08 +0100

    remove main file

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 KTH Royal Institute of Technology Sweden
+#
+# invenio-config-kth is free software, you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file details.
+
+
+def main():
+    """main entry point"""
+    print("test")
+
+
+if __name__ == "__main__":
+    main()
